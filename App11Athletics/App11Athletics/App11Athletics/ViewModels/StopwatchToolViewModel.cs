d1de7f2036f92dcd6dd3053033a57198c06357d5
dorian@dhcapture.com <dorian@dhcapture.com>
2017-01-14 07:58:45 -0500

    SW View Model Commands set (start, stop, reset);

--- App11Athletics/App11Athletics/App11Athletics/ViewModels/StopwatchToolViewModel.cs
+++ App11Athletics/App11Athletics/App11Athletics/ViewModels/StopwatchToolViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -14,22 +16,64 @@ namespace App11Athletics.ViewModels
     {
         public StopwatchToolViewModel()
         {
+            // Initialize any properties
             TimerDateTime = TimeSpan.Zero;
-            StartTimerCommand = new Command(async () => { await StartTimerAsync(); });
-            StopTimerCommand = new Command(async () => { await StopTimerAsync(); });
-            ResetTimerCommand = new Command(async () => { await ResetTimerAsync(); });
+
+            // Initialize your Commands
+            StartTimerCommand = new Command(execute: async () =>
+            {
+                TimerRunning = true;
+                await StartTimerAsync();
+                 RefreshCanExecutes(); 
+            }, canExecute: () => !TimerRunning);
+
+            StopTimerCommand = new Command( () =>
+                {
+                    TimerRunning = false;
+                    StopTimer();
+                    RefreshCanExecutes(); 
+                }, canExecute:
+                () => TimerRunning);
+        
+            ResetTimerCommand = new Command(async () =>
+            {
+                TimerRunning = false;
+                ResetTimer();
+                RefreshCanExecutes();
+            }, () => !Reset || TimerRunning);
         }
 
+        private void RefreshCanExecutes()
+        {
+            ((Command)StartTimerCommand).ChangeCanExecute();
+            ((Command)StopTimerCommand).ChangeCanExecute();
+            ((Command)ResetTimerCommand).ChangeCanExecute();
+        }
+
+        #region Properties
+
         public DateTime StartDateTime { get; set; }
+        public TimeSpan ResumeDateTime { get; set; }
         public TimeSpan TimerDateTime { get; set; }
+       
         public bool TimerRunning { get; set; }
-        public bool Reset { get; set; }
+        public bool Reset => TimerDateTime == TimeSpan.Zero;
+        public bool StartNew => true;
+        public bool Resume => true;
+
+        #endregion
+
+        #region Implementation of ICommand
 
         public ICommand StartTimerCommand { get; set; }
 
         public ICommand StopTimerCommand { get; set; }
 
         public ICommand ResetTimerCommand { get; set; }
+
+        #endregion
+
+        #region Methods
 
         private async Task StartTimerAsync()
         {
@@ -38,43 +82,51 @@ namespace App11Athletics.ViewModels
 
         private void StartTimer()
         {
-            StartDateTime = DateTime.Now;
-            TimerRunning = true;
+            // Check if the timer is already reset
+            StartDateTime = Reset ? DateTime.Now : DateTime.Now.Subtract(ResumeDateTime);
+            
             Device.StartTimer(TimeSpan.FromMilliseconds(15), OnTimerTick);
+        }
+
+        private bool OnTimerTick()
+        {
+            if (TimerRunning)
+            {
+                TimerDateTime = DateTime.Now.Subtract(StartDateTime);
+            }
+            return TimerRunning;
         }
 
         private async Task StopTimerAsync()
         {
+
+
             await Task.Run(() => StopTimer());
         }
 
         public void StopTimer()
         {
-            TimerDateTime = TimerDateTime;
-            TimerRunning = false;
+            ResumeDateTime = TimerDateTime;
         }
 
         private async Task ResetTimerAsync()
         {
-          var t = await Task.Run(() =>TimerRunning = false);
-           ResetTimer();
+          
+           await Task.Run(() => ResetTimer());
+                
             
         }
 
         private void ResetTimer()
-        {  
-            TimerDateTime = TimeSpan.Zero;
-        }
-
-        private bool OnTimerTick()
         {
-            TimerDateTime = DateTime.Now.Subtract(StartDateTime);
-            return TimerRunning;
+            TimerDateTime = TimeSpan.Zero;
         }
 
         #region Implementation of INotifyPropertyChanged
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
 
         #endregion
     }

5844f27f10116cd0986d3ab7264c80bcaddab863
dorian@dhcapture.com <dorian@dhcapture.com>
2017-03-15 14:53:00 -0400

    successful release

--- App11Athletics/App11Athletics/App11Athletics/Views/SplashWebView.xaml.cs
+++ App11Athletics/App11Athletics/App11Athletics/Views/SplashWebView.xaml.cs
@@ -12,7 +12,13 @@ namespace App11Athletics.Views
         public SplashWebView()
         {
             InitializeComponent();
+            var c = Plugin.Connectivity.CrossConnectivity.Current.IsConnected;
+            if(c)
             videoPlayer.Play();
+            else
+            {
+                VideoPlayer_OnCompleted(null,null);
+            }
             //            var htmlSource = new HtmlWebViewSource();
             //            htmlSource.Html = @"<iframe src='https://player.vimeo.com/video/206658139?autoplay=1&title=0&byline=0&portrait=0' width='100%' height='100%' frameborder='0' ></iframe>";
             //
